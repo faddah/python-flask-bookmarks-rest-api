e357f0ad4c02dc881707ab398a3b549f4efbca7c
Faddah Wolf <my_biz@me.com>
2025-02-12 13:31:41 -0800

    Added imports for Blueprint, jsonify, request, werkzeug (generate_password_hash), and http_status_codes needed.

--- src/auth.py
+++ src/auth.py
@@ -1,5 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request  # type: ignore
+from werkzeug.security import generate_password_hash  # type: ignore
 
+from src.constants.http_status_codes import (
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_409_CONFLICT,
+)
+from src.database import User, db
 
 auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
 
