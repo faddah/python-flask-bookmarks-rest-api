d7e6a482539e5bfa2a88bd82050cbfc416e19e6a
Faddah Wolf <my_biz@me.com>
2025-02-15 17:30:28 -0800

    In app.config added JWT_SECRET_KEY that is in .env.

--- src/__init__.py
+++ src/__init__.py
@@ -20,6 +20,7 @@ def create_app(test_config=None):
             SECRET_KEY=os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY"),
         )
     else:
         app.config.from_mapping(test_config)
