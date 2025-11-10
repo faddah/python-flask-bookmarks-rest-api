e10872d9171926a422351e1f08c688d59ab42f23
Faddah Wolf <my_biz@me.com>
2025-11-09 16:17:47 -0800

    Added Doc strings for Python Linter; also added code to do the Short URLs as that was not working.

--- src/database.py
+++ src/database.py
@@ -1,3 +1,4 @@
+"""Database models for User and Bookmark."""
 # from enum import unique
 from datetime import datetime
 import string
@@ -8,6 +9,7 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
+    """User model for storing user account information."""
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.Text(), unique=True, nullable=False)
@@ -16,11 +18,16 @@ class User(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=datetime.now())
     bookmarks = db.relationship("Bookmark", backref="user")
 
+    def get_bookmarks_count(self):
+        """Return the count of bookmarks for this user."""
+        return len(self.bookmarks)
+
     def __repr__(self):
         return f"User>>> {self.username}"
 
 
 class Bookmark(db.Model):
+    """Bookmark model for storing user bookmarks with short URLs."""
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text(), nullable=True)
     url = db.Column(db.Text(), nullable=False)
@@ -31,17 +38,22 @@ class Bookmark(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=datetime.now())
 
     def generate_short_characters(self):
+        """Generate a unique 3-character short URL code."""
         characters = string.digits + string.ascii_letters
         picked_chars = "".join(random.choices(characters, k=3))
 
         link = self.query.filter_by(short_url=picked_chars).first()
 
-        if not link:
+        if link:
+            # If collision exists, generate a new one recursively
+            return self.generate_short_characters()
+        else:
             return picked_chars
 
     def __init__(self, **kwargs):
+        """Initialize bookmark with auto-generated short URL."""
         super().__init__(**kwargs)
-        self.generate_short_characters()
+        self.short_url = self.generate_short_characters()
 
     def __repr__(self):
         return f"Bookmark>>> {self.url}"
